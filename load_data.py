feat(Core): ensemble models, correct forward returns calculation, scaling, only train from when asset returns are available, major bug fixed in walk_forward_train_test (#35)
* fix(Core): correct forward returns calculation, classifiers are now working again, only train from when asset returns are available

* feat(Utils): added get_first_valid_return_index()

* feat(Ensemble): return models from `run_whole_pipeline`

* feat(Ensemble): added ensemble step, fixed walk_forward_train_test predictions index confusion,

* chore(Pipeline): remove unnecessary extra ensemble results dataframe

* refactor(Core): removed unnecessary ensemble_train_predict, moved run_single_asset_trainig_pipeline to a separate file

* feat(Training): added scaling on expanding window (the past) to walk_forward_train_test(), now printing out mean sharpe ratio

* feat(CI): added environment.yml file

* chore(Environment): update env.yml

* feat(CI): added testing workflow

* fix(CI): renamed enviroment.yml

* fix(Tests): added missing new parameter to walk_forward_train_test()

load_data.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import OneHotEncoder
 #%%
 
 def get_crypto_assets(path: str) -> list[str]:
-    return sorted([f.split('.')[0] for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and '_' in f and not f.startswith('.')])
+    return sorted([f.split('.')[0] for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and 'USD' in f and not f.startswith('.')])
 
 def get_etf_assets(path: str) -> list[str]:
     return sorted([f.split('.')[0] for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and '_' not in f and not f.startswith('.')])
@@ -29,7 +29,14 @@ def load_data(path: str,
             index_column: Literal['date', 'int'],
             method: Literal['regression', 'classification'],
             narrow_format: bool = False,
-        ) -> tuple[pd.DataFrame, pd.Series]:
+        ) -> tuple[pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Loads asset data from the specified path.
+    Returns:
+        - DataFrame `X` with all the training data
+        - Series `y` with the target asset returns shifted by 1 day OR if it's a classification problem, the target class)
+        - Series `forward_returns` with the target asset returns shifted by 1 day
+    """
     
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and not f.startswith('.')]
     files = [f for f in files if load_other_assets == True or (load_other_assets == False and f.startswith(target_asset))]
@@ -37,7 +44,7 @@ def load_data(path: str,
     dfs = [__load_df(
         path=os.path.join(path,f),
         prefix=f.split('.')[0],
-        log_returns=log_returns,
+        returns='log_returns' if log_returns else 'returns',
         technical_features=own_technical_features if is_target_asset(target_asset, f) else other_technical_features,
         lags= target_asset_lags if is_target_asset(target_asset, f) else other_asset_lags,
         narrow_format=narrow_format,
@@ -63,28 +70,60 @@ def load_data(path: str,
     ## Create target 
     target_col = 'target'
     returns_col = target_asset + '_returns'
+    prediction_horizon = 1
+    forward_returns = __create_target_cum_forward_returns(dfs, returns_col, prediction_horizon)
     if method == 'regression':
-        dfs = __create_target_cum_forward_returns(dfs, returns_col, 1)
+        dfs[target_col] = forward_returns
     elif method == 'classification':
-        dfs = __create_target_classes(dfs, returns_col, 1, 'two')
-        
+        dfs[target_col] = __create_target_classes(dfs, returns_col, prediction_horizon, 'two')
+    # we need to drop the last row, because we forward-shift the target (see what happens if you call .shift[-1] on a pd.Series) 
+    dfs = dfs.iloc[:-prediction_horizon]
+    forward_returns = forward_returns.iloc[:-prediction_horizon]
+    
     X = dfs.drop(columns=[target_col])
     y = dfs[target_col]
 
-    return X, y
+    return X, y, forward_returns
 
-def __load_df(path: str, prefix: str, log_returns: bool, technical_features: Literal['none', 'level1', 'level2'], lags: list[int], narrow_format: bool = False) -> pd.DataFrame:
+# %%
+
+def load_crypto_only_returns(path: str, index_column: Literal['date', 'int'], returns: Literal['price', 'returns']) -> pd.DataFrame:
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and 'USD' in f and not f.startswith('.')]
+    dfs = [__load_df(
+        path=os.path.join(path,f),
+        prefix=f.split('.')[0],
+        returns=returns,
+        technical_features='none',
+        lags=[],
+        narrow_format=False,
+    ) for f in files]
+    dfs = pd.concat(dfs, axis=1)
+    dfs = dfs.applymap(lambda x: np.nan if x == 0 else x)
+    dfs.index = pd.DatetimeIndex(dfs.index)
+    dfs.columns = [column.split('_')[0] for column in dfs.columns]
+    if index_column == 'int':
+        dfs.reset_index(drop=True, inplace=True)
+
+    return dfs
+
+def load_crypto_assets_availability(path: str, index_column: Literal['date', 'int']) -> pd.DataFrame:
+    return load_crypto_only_returns(path, index_column, 'returns').applymap(lambda x: 0 if x == 0.0 or x == 0 or np.isnan(x) else 1)
+
+
+def __load_df(path: str, prefix: str, returns: Literal['price', 'returns', 'log_returns'], technical_features: Literal['none', 'level1', 'level2'], lags: list[int], narrow_format: bool = False) -> pd.DataFrame:
     df = pd.read_csv(path, header=0, index_col=0).fillna(0)
 
-    if log_returns:
+    if returns == 'log_returns':
         df['returns'] = np.log(df['close']).diff(1)
+    elif returns == 'price':
+        df['returns'] = df['close']
     else:
         df['returns'] = df['close'].pct_change()
     
     for lag in lags:
         df[f'lag_{lag}'] = df['returns'].shift(lag)
 
-    df = __augment_derived_features(df, log_returns=log_returns, technical_features=technical_features)
+    df = __augment_derived_features(df, log_returns=True if returns == 'log_returns' else False, technical_features=technical_features)
 
     df = df.replace([np.inf, -np.inf], 0.)
     df = df.drop(columns=['open', 'high', 'low', 'close'])
@@ -100,7 +139,7 @@ def __load_df(path: str, prefix: str, log_returns: bool, technical_features: Lit
 
 def __augment_derived_features(df: pd.DataFrame, log_returns: bool, technical_features: Literal['none', 'level1', 'level2']) -> pd.DataFrame:
     if technical_features == 'level1' or technical_features == 'level2':
-        # volatility (10, 20, 30 days)
+        # volatility (10, 20, 30, 60 days)
         df['vol_10'] = df['returns'].rolling(10).std()*(252**0.5)
         df['vol_20'] = df['returns'].rolling(20).std()*(252**0.5)
         df['vol_30'] = df['returns'].rolling(30).std()*(252**0.5)
@@ -137,16 +176,15 @@ def __augment_derived_features(df: pd.DataFrame, log_returns: bool, technical_fe
     return df
 
 
-# %%
 
 # %%
-def __create_target_cum_forward_returns(df: pd.DataFrame, source_column: str, period: int) -> pd.DataFrame:
-    df['target'] = df[source_column].diff(period).shift(-period)
-    df = df.iloc[:-period]
-    return df
+def __create_target_cum_forward_returns(df: pd.DataFrame, source_column: str, period: int) -> pd.Series:
+    assert period > 0
+    return df[source_column].shift(-period)
 
 
-def __create_target_classes(df: pd.DataFrame, source_column: str, period: int, no_of_classes: Literal["two", "three"]) -> pd.DataFrame:
+def __create_target_classes(df: pd.DataFrame, source_column: str, period: int, no_of_classes: Literal["two", "three"]) -> pd.Series:
+    assert period > 0
 
     def get_class_binary(x):
         return 0 if x <= 0.0 else 1
@@ -162,16 +200,10 @@ def __create_target_classes(df: pd.DataFrame, source_column: str, period: int, n
         else:
             return 1
 
-    if period > 0:
-        df['target'] = df[source_column].shift(-period)
-    else:
-        df['target'] = df[source_column]
+    target_column = df[source_column].shift(-period)
     
     get_class_function = get_class_binary
     if no_of_classes == "three":
         get_class_function = get_class_threeway
 
-    df['target'] = df['target'].map(get_class_function)
-    if period > 0:
-        df = df.iloc[:-period]
-    return df
+    return target_column.map(get_class_function)

tests/test_walk_forward.py
@@ -1,20 +1,21 @@
 import pytest
 import numpy as np
 import pandas as pd
-from utils.walk_forward import walk_forward_train_test
+from training.walk_forward import walk_forward_train_test
 from sklearn.base import BaseEstimator
 
-def __generate_test_data():
+no_of_rows = 100
+
+def __generate_test_data(no_of_rows):
     no_columns = 6
-    no_rows = 100
-    X = [[row] * no_columns for row in range(no_rows)]
+    X = [[row] * no_columns for row in range(no_of_rows)]
     assert X[0][0] == 0
     assert X[1][0] == 1
     assert X[2][0] == 2
     assert X[3][0] == 3
     X = pd.DataFrame(X)
 
-    y = [row+1 for row in range(no_rows)]
+    y = [row+1 for row in range(no_of_rows)]
     assert y[0] == 1
     assert y[1] == 2
     assert y[2] == 3
@@ -24,7 +25,7 @@ def __generate_test_data():
 
 
 def test_walk_forward_train_test():
-    X, y = __generate_test_data()
+    X, y = __generate_test_data(no_of_rows)
 
     window_length = 10
     class StubModel(BaseEstimator):
@@ -38,4 +39,16 @@ def test_walk_forward_train_test():
             return np.array([X[0][0] + 1])
 
     model = StubModel()
-    walk_forward_train_test('test', model, X, y, window_length, 10)
+    scaler = None
+    models, predictions = walk_forward_train_test(
+        model_name='test',
+        model=model,
+        X=X,
+        y=y,
+        target_returns=y,
+        window_size=window_length,
+        retrain_every=10,
+        scaler=scaler)
+    for i in range(window_length, no_of_rows):
+        predictions[i] == y[i]
+    

utils/evaluate.py
@@ -1,53 +1,63 @@
 from typing import Literal
 from sklearn.metrics import mean_absolute_error, accuracy_score, r2_score, f1_score, precision_score, recall_score
 from quantstats.stats import expected_return, sharpe, skew, sortino
+from utils.helpers import get_first_valid_return_index
 import pandas as pd
 import numpy as np
 
-def backtest(returns: pd.Series, signal: pd.Series, transaction_cost = 0.02) -> pd.Series:
+def backtest(returns: pd.Series, signal: pd.Series, transaction_cost = 0.01) -> pd.Series:
     delta_pos = signal.diff(1).abs().fillna(0.)
     costs = transaction_cost * delta_pos
     return (signal * returns) - costs
 
 
-def __preprocess(y_true: pd.Series, y_pred: pd.Series, method: Literal['classification', 'regression']):
+def __preprocess(target_returns: pd.Series, y_pred: pd.Series, method: Literal['classification', 'regression']) -> pd.DataFrame:
     y_pred.name = 'y_pred'
-    y_true.name = 'y_true'
-    df = pd.concat([y_pred, y_true],axis=1).dropna()
+    target_returns.name = 'target_returns'
+    df = pd.concat([y_pred, target_returns],axis=1).dropna()
 
-    if method == 'regression':
-        df['sign_pred'] = df.y_pred.apply(np.sign)
-    else:
-        df['sign_pred'] = df.y_pred.apply(lambda x: 1 if x>0 else -1)
-    df['sign_true'] = df.y_true.apply(np.sign)
+    df['sign_pred'] = df.y_pred.apply(lambda x: 1 if x>0 else -1)
+    def sign_true(x):
+        if x > 0:
+            return 1
+        else:
+            return -1
+    df['sign_true'] = df.target_returns.apply(sign_true)
     df['is_correct'] = 0
     df.loc[df.sign_pred * df.sign_true > 0 ,'is_correct'] = 1 # only registers 1 when prediction was made AND it was correct
     df['is_incorrect'] = 0
     df.loc[df.sign_pred * df.sign_true < 0,'is_incorrect'] = 1 # only registers 1 when prediction was made AND it was wrong
     df['is_predicted'] = df.is_correct + df.is_incorrect
-    df['result'] = backtest(df.y_true, df.sign_pred)
+    df['result'] = backtest(df.target_returns, df.sign_pred)
 
     return df
 
-def evaluate_predictions(model_name: str, y_true: pd.Series, y_pred: pd.Series, sliding_window_size: int, method: Literal['classification', 'regression']):
+def evaluate_predictions(
+                        model_name: str,
+                        target_returns: pd.Series,
+                        y_pred: pd.Series,
+                        sliding_window_size: int,
+                        method: Literal['classification', 'regression']
+                        ) -> pd.Series:
     # ignore the predictions until we see a non-zero returns (and definitely skip the first sliding_window_size)
-    first_nonzero_return = np.where(y_true != 0)[0][0]
+    first_nonzero_return = get_first_valid_return_index(target_returns)
     evaluate_from = first_nonzero_return + sliding_window_size + 1
     
-    y_true = pd.Series(y_true[evaluate_from:])
-    # if there are lots of zeros in the ground truth returns, probably something is wrong, but we can tolerate a couple of days of missing data.
-    is_zero = y_true[y_true == 0]
-    assert len(is_zero) < 5
-    # we can't deal with 0 returns, so we'll just remap the few examples to 1
-    y_true = y_true.apply(lambda x: 1 if x == 0 else x)
+    target_returns = pd.Series(target_returns[evaluate_from:])
+    if method == 'regression':
+        # if there are lots of zeros in the ground truth returns, probably something is wrong, but we can tolerate a couple of days of missing data.
+        is_zero = target_returns[target_returns == 0]
+        assert len(is_zero) < 15
+    # we can't deal with 0 returns, so we'll just remap the few examples to 0.0001 
+    target_returns = target_returns.apply(lambda x: 0.0001 if x == 0 else x)
     y_pred = pd.Series(y_pred[evaluate_from:])
 
-    df = __preprocess(y_true, y_pred, method)
+    df = __preprocess(target_returns, y_pred, method)
     
     scorecard = pd.Series()
     if method == 'regression':
-        scorecard.loc['RSQ'] = r2_score(df.y_true, df.y_pred)
-        scorecard.loc['MAE'] = mean_absolute_error(df.y_true, df.y_pred)
+        scorecard.loc['RSQ'] = r2_score(df.target_returns, df.y_pred)
+        scorecard.loc['MAE'] = mean_absolute_error(df.target_returns, df.y_pred)
     elif method == 'classification':
         scorecard.loc['RSQ'] = 0.
         scorecard.loc['MAE Matrix'] = 0.